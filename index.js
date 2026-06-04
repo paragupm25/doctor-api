95c8bcd53f530027b5ef9f6d10b568d98c551b30
paragupm25 <parag.paul@alumnos.upm.es>
2026-06-04 15:49:24 +0600

    Implement CORS support in index.js
    Add CORS middleware to handle preflight requests

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 // In-memory OTP store
 const otpStore = {};
 
